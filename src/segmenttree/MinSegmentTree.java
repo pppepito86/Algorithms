6ce9d9fccf8aaf10d74f4b60406104fa38eca07c
Petar Petrov <pppepito86@gmail.com>
2016-12-24 00:14:22 +0200

    segment tree

---
+++ src/segmenttree/MinSegmentTree.java
@@ -0,0 +1,50 @@
+package segmenttree;
+
+import java.util.Arrays;
+
+public class MinSegmentTree {
+	
+	private int size;
+	private long[] values;
+
+	public MinSegmentTree(int n) {
+		this.size = n;
+		this.values = new long[2*size];
+		Arrays.fill(values, Integer.MAX_VALUE);
+	}
+	
+	public void replace(int element, int value) {
+		updateInternal(1, 1, size, element, value, true);
+	}
+	
+	public void increase(int element, int value) {
+		updateInternal(1, 1, size, element, value, false);
+	}
+	
+	public long min(int i, int j) {
+		return minInternal(1, 1, size, i, j);
+	}
+	
+	private void updateInternal(int node, int left, int right, int element, int value, boolean replace) {
+		if (element < left || element > right) return;
+		if (left == right) { 
+			if (replace) values[node] = value; 
+			else values[node] += value;
+			return; 
+		}
+		
+		int mid = (left + right) / 2;
+		updateInternal(2*node, left, mid, element, value, replace);
+		updateInternal(2*node+1, mid+1, right, element, value, replace);
+		values[node] = Math.min(values[2*node], values[2*node+1]);
+	}
+
+	private long minInternal(int node, int left, int right, int i, int j) {
+		if (j < left || i > right) return Integer.MAX_VALUE;
+		if (i <= left && right <= j) return values[node];
+		
+		int mid = (left + right) / 2;
+		return Math.min(minInternal(2*node, left, mid, i, j), minInternal(2*node+1, mid+1, right, i, j));
+	}
+	
+}
---
+++ src/segmenttree/SegmentTree.java
@@ -0,0 +1,62 @@
+package segmenttree;
+
+public class SegmentTree {
+	
+	interface MathOperation {
+		long operation(long a, long b);
+	}
+	public static MathOperation ADDITION = (long a, long b) -> a + b;
+	public static MathOperation MINIMUM = (long a, long b) -> Math.min(a, b);
+	
+	private int size;
+	private int defaultValue;
+	private long[] values;
+	private MathOperation operation;
+
+	public SegmentTree(int n, MathOperation operation, int defaultValue) {
+		this.size = n;
+		this.operation = operation;
+		this.defaultValue = defaultValue;
+		this.values = new long[2*size];
+		if (defaultValue != 0) {
+			for (int i = 1; i <= n; i++) replace(i, defaultValue);
+		}
+	}
+	
+	public void replace(int element, int value) {
+		updateInternal(1, 1, size, element, value, true);
+	}
+	
+	public void increase(int element, int value) {
+		updateInternal(1, 1, size, element, value, false);
+	}
+	
+	public long compute(int i, int j) {
+		return computeInternal(1, 1, size, i, j);
+	}
+	
+	private void updateInternal(int node, int left, int right, int element, int value, boolean replace) {
+		if (element < left || element > right) return;
+		if (left == right) { 
+			if (replace) values[node] = value; 
+			else values[node] += value;
+			return; 
+		}
+		
+		int mid = (left + right) / 2;
+		updateInternal(2*node, left, mid, element, value, replace);
+		updateInternal(2*node+1, mid+1, right, element, value, replace);
+		values[node] = operation.operation(values[2*node], values[2*node+1]);
+	}
+
+	private long computeInternal(int node, int left, int right, int i, int j) {
+		if (j < left || i > right) return defaultValue;
+		if (i <= left && right <= j) return values[node];
+		
+		int mid = (left + right) / 2;
+		return operation.operation(
+				computeInternal(2*node, left, mid, i, j),
+				computeInternal(2*node+1, mid+1, right, i, j));
+	}
+	
+}
