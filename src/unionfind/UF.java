f01d89a1e8560aa10a9760af5565dbab6aafe3be
Petar Petrov <pppepito86@gmail.com>
2016-12-23 17:35:47 +0200

    union find

---
+++ src/unionfind/UF.java
@@ -0,0 +1,38 @@
+package unionfind;
+
+import java.util.Arrays;
+
+public class UF {
+
+	int count;
+	int[] parent, size;
+
+	public UF(int n) {
+		count = n; parent = new int[n]; size = new int[n];
+		for (int i = 0; i < n; i++) parent[i] = i;
+		Arrays.fill(size, 1);
+	}
+	
+	int find(int u){
+		return parent[u] = parent[u]==u?u:find(u);
+	}
+	
+	void union(int u, int v) {
+		int ru = find(u); int rv = find(v);
+		if (ru == rv) return;
+		if (size[ru] >= size[rv]) {
+			parent[rv] = ru; size[ru] += size[rv];
+		} else {
+			parent[ru] = rv; size[rv] += size[ru];
+		}
+		count--;
+	}
+	
+	boolean connected(int u, int v) {
+		return find(u) == find(v);
+	}
+	
+    public int count() {
+        return count;
+    }
+}
